859ab73fee09152d0dc974f253c81afae5bf9866
Anton Matiash <amatiash.dev@gmail.com>
2025-07-31 18:27:22 +0400

    Added rules for eslint-plugin-unicorn

---
+++ for/unicorn.ts
@@ -0,0 +1,14 @@
+// noinspection JSUnusedGlobalSymbols
+
+import type { Linter } from 'eslint'
+
+export const paperRulesForUnicorn: Readonly<Linter.RulesRecord> = {
+    'unicorn/filename-case': 'off', // Don't restrict file naming
+    'unicorn/no-null': 'off', // null is ok
+    'unicorn/prevent-abbreviations': 'off', // Don't restrict abbreviations
+    'unicorn/consistent-function-scoping': 'off', // Don't restrict function scoping
+    'unicorn/explicit-length-check': 'off', // Disable due to a false positive with context.size in React.useContext()
+    'unicorn/prefer-global-this': 'off', // Disable - window is ok for now
+    'unicorn/numeric-separators-style': 'off', // Disable - numeric separators are not needed here
+    'unicorn/prefer-top-level-await': 'off', // Disable - top-level await is supported from ES2022
+}
